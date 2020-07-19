c2c791f52f7876fd8061b88d80e21d8153299d08
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 19:10:14 +0300

    Добавьте файлы проекта.

---
+++ 24012020dz4.cpp
@@ -0,0 +1,30 @@
+﻿/*
+4. Составить программу, которая по данному числу(1—12) выводит название соответствующего ему месяца.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int a;
+	cout << "Введите число: ";
+	cin >> a;
+	switch (a) {
+	case 1:cout << "Январь"; break;
+	case 2:cout << "Февраль"; break;
+	case 3:cout << "Март"; break;
+	case 4:cout << "Апрель"; break;
+	case 5:cout << "Май"; break;
+	case 6:cout << "Июнь"; break;
+	case 7:cout << "Июль"; break;
+	case 8:cout << "Август"; break;
+	case 9:cout << "Сентябрь"; break;
+	case 10:cout << "Октябрь"; break;
+	case 11:cout << "Ноябрь"; break;
+	case 12:cout << "Декабрь"; break;
+	default: cout << "Ошибка";
+	}
+	return 0;
+}
